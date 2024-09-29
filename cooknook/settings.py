4268809436dc23fb34a332d92273c11b7736196e
urmarc443 <urmarc443@gmail.com>
2024-09-29 10:10:11 -0700

    ADD .gitignore AND UPDATE settings.py with os env password

--- cooknook/settings.py
+++ cooknook/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from os import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'cooknook',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': os.environ['DBPASS'],
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
